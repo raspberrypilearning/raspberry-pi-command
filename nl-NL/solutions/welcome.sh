760b06fcc8789b1c58c2a744e069b2f4ede29562
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-03-26 15:15:57 +0000

    New translations welcome.sh (Dutch)

--- nl-NL/solutions/welcome.sh
+++ nl-NL/solutions/welcome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\e[33mWelcome to \e[1;33m$HOSTNAME"
+echo -e "\e[33mWelkom bij \e[1;33m$HOSTNAME"
 sleep 1
-echo -e "\e[0;35mIt's \e[1;35m$(date +%A)"
+echo -e "\e[0;35mHet is \e[1;35m$(date +%A)"
 read -n 1
